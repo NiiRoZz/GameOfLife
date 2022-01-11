4b0f41789d61cfcf0008534427b41a607d0bc9f8
NiiRoZz <extazziii1000@gmail.com>
2022-01-11 17:34:39 +0100

    Add default screen

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <gf/Window.h>
+#include <gf/Color.h>
+#include <gf/RenderWindow.h>
+#include <gf/Event.h>
+
+int main(int , char const **)
+{
+    gf::Window window("GameOfLife", { 640, 480 });
+    gf::RenderWindow renderer(window);
+
+    renderer.clear(gf::Color::Black);
+
+    while (window.isOpen())
+    {
+        //update
+        gf::Event event;
+
+        while (window.pollEvent(event))
+        {
+            switch (event.type)
+            {
+                case gf::EventType::Closed:
+                {
+                    window.close();
+                    break;
+                }
+            }
+        }
+
+        renderer.clear();
+
+        //render
+
+        renderer.display();
+    }
+
+    return 0;
+}
